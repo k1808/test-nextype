eed11272c752db7e08e0e7f813bc542330c0223c
Evgeniy Malashenko <111k1808@gmail.com>
2020-09-03 15:43:38 +0300

    adds the first additional task

---
+++ classes/cbase.php
@@ -0,0 +1,42 @@
+<?php
+
+set_exception_handler(function ($exception)
+{
+    echo "Exception: " . $exception->getMessage();
+});
+
+class CBase
+{
+    private static $params;
+    private static $instance;
+
+    public function __construct($params = false)
+    {
+        if (is_array($params))
+            self::$params = $params;
+    }
+
+    public static function getInstance($param)
+    {
+        if (is_null(self::$instance))
+        {
+            self::$instance = new self($param);
+        }
+
+        return self::$instance;
+    }
+
+    public static function getParam($property)
+    {
+        if (!is_null(self::$params) && isset(self::$params[$property]))
+            return self::$params[$property];
+
+        throw new \Exception('Oops, there is no such property!');
+    }
+    public function setParams($params)
+    {
+        $this->params = $params;
+    }
+
+
+}
---
+++ indexDop1.php
@@ -0,0 +1,4 @@
+<?php
+require_once "classes/autoload.php";
+
+echo CBase::getParam('message');
